b74a3e30b604e731652375742af66411d96d3b68
Bruno Bossola <bbossola@gmail.com>
2016-09-21 13:41:59 +0100

    [1] Structure compatible with pip repos

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ppybot',
+  packages = ['ppybot'],
+  version = '0.1',
+  description = 'A pybot orchestrator capable to run pybot suite tests concurrently',
+  author = 'Bruno Bossola',
+  author_email = 'bbossola@gmail.com',
+  url = 'https://github.com/bbossola/parallel_pybot', 
+  download_url = 'https://github.com/bbossola/parallel_pybot/tarball/0.1', 
+  keywords = ['pybot', 'testing', 'parallel'], 
+  classifiers = [],
+)
